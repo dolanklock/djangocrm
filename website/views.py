a4b5f2e7d4c7733a10b35ec8f4ff4ff40e8368d9
Dolan Klock <dolank16@gmail.com>
2023-09-18 21:14:20 -0400

    added decorator for login auth

--- website/views.py
+++ website/views.py
@@ -3,6 +3,14 @@ from . import forms, models
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 
+def login_auth(fn):
+    def wrapper(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return fn(request)
+        else:
+            return redirect('home')
+    return wrapper
+
 def home(request):
     if request.method == "POST":
         username = request.POST["username"]
@@ -19,70 +27,60 @@ def home(request):
     else:
         return render(request, 'home.html', {})
 
+@login_auth
 def table(request):
-    if request.user.is_authenticated:
-        all_data = models.ModelCRM.objects.all()
-        return render(request, 'table.html', {'all_data': all_data})
-    else:
-        return redirect('home')
+    all_data = models.ModelCRM.objects.all()
+    return render(request, 'table.html', {'all_data': all_data})
 
+@login_auth
 def update(request, item_id):
-    if request.user.is_authenticated:
-        item = models.ModelCRM.objects.get(pk=item_id)
-        return render(request, 'update.html', {"item": item})
-    else:
-        return redirect('home')
+    item = models.ModelCRM.objects.get(pk=item_id)
+    return render(request, 'update.html', {"item": item})
 
+@login_auth
 def updateinfo(request, item_id):
-    if request.user.is_authenticated:
-        if request.method == "POST":
-            item = models.ModelCRM.objects.get(pk=item_id)
-            item.first_name = request.POST["first_name"]  # saving the items parameter 'first_name' with new value typed in from html and webpage
-            item.last_name = request.POST["last_name"]
-            item.email = request.POST["email"]
-            item.address = request.POST["address"]
-            item.city = request.POST["city"]
-            item.province = request.POST["province"]
-            item.postal_code = request.POST["postal_code"]
-            item.save()  # saves to database
-            messages.success(request,('Customer records successfully updated'))  # this message gets listed on the admin page inside the new database
-            return redirect('table')
-        else:
-            item = models.ModelCRM.objects.get(pk=item_id)
-            if request.user.is_authenticated:
-                return render(request, 'updateinfo.html', {"item": item})
-    else:
-        return redirect('home')
-    
-def addcustomer(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
-            first_name = request.POST["first_name"]
-            last_name = request.POST["last_name"]
-            email = request.POST["email"]
-            address = request.POST["address"]
-            city = request.POST["city"]
-            province = request.POST["province"]
-            postal_code = request.POST["postal_code"]
-            model = models.ModelCRM(first_name=first_name, last_name=last_name, email=email,
-                                   address=address, city=city, province=province, postal_code=postal_code)  # sets 'form' equal to what was typed in to input on webpage
-            model.save()  # saves to database
-            messages.success(request,('Customer has been added successfully'))  # this message gets listed on the admin page inside the new database
-            # return render(request, 'table.html', {})
-            return redirect('table')
-        else:
-            return render(request, 'addcustomer.html', {})
-    else:
-        return redirect('home')
-
-def deletecustomer(request, item_id):
-    if request.user.is_authenticated:
+    if request.method == "POST":
         item = models.ModelCRM.objects.get(pk=item_id)
-        item.delete()
-        messages.success(request, "Customer removed")
+        item.first_name = request.POST["first_name"]  # saving the items parameter 'first_name' with new value typed in from html and webpage
+        item.last_name = request.POST["last_name"]
+        item.email = request.POST["email"]
+        item.address = request.POST["address"]
+        item.city = request.POST["city"]
+        item.province = request.POST["province"]
+        item.postal_code = request.POST["postal_code"]
+        item.save()  # saves to database
+        messages.success(request,('Customer records successfully updated'))  # this message gets listed on the admin page inside the new database
         return redirect('table')
     else:
-        return redirect('home')
+        item = models.ModelCRM.objects.get(pk=item_id)
+        if request.user.is_authenticated:
+            return render(request, 'updateinfo.html', {"item": item})
+
+@login_auth
+def addcustomer(request):
+    if request.method == "POST":
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        email = request.POST["email"]
+        address = request.POST["address"]
+        city = request.POST["city"]
+        province = request.POST["province"]
+        postal_code = request.POST["postal_code"]
+        model = models.ModelCRM(first_name=first_name, last_name=last_name, email=email,
+                                address=address, city=city, province=province, postal_code=postal_code)  # sets 'form' equal to what was typed in to input on webpage
+        model.save()  # saves to database
+        messages.success(request,('Customer has been added successfully'))  # this message gets listed on the admin page inside the new database
+        # return render(request, 'table.html', {})
+        return redirect('table')
+    else:
+        return render(request, 'addcustomer.html', {})
+  
+@login_auth
+def deletecustomer(request, item_id):
+    item = models.ModelCRM.objects.get(pk=item_id)
+    item.delete()
+    messages.success(request, "Customer removed")
+    return redirect('table')
 
 def logout_user(request):
     logout(request)
